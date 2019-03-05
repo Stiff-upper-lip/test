0a5a94be7cc0df68c3cbfb51f33e3d4d20f0d939
Andrew <adept@mail.ru>
2019-03-06 00:28:57 +0200

    This is my first

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+let person = {
+    name: "Hommy",
+    age: 23,
+    isMaried: false
+};
+// console.log(person.isMaried);
+let answer = prompt("Hells Bells","fuck","");
+console.log(answer);
