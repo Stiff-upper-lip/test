e0ac227ceaa93ea14f0dd72a3b4bf6d0b859b4a8
Andrew <adept@mail.ru>
2019-03-14 17:44:38 +0200

    ЭТО ПРОБНЫЙ КОММИТ

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
 'use strict';
 
 
+var money = prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату и время в формате", "YYYY-MM-DD");
 
-let person = {
-    name: "Hommy",
-    age: 23,
-    isMaried: false
+var addData = {
+    budzet: money,
+    itsTime: time
+
 };
+
+console.log(addData.budzet);
+console.log(addData.time);
+
+
+
+
+
+
+
+// let person = {
+//     name: "Hommy",
+//     age: 23,
+//     isMaried: false
+// };
 // console.log(person.isMaried);
-let answer = prompt("Hells Bells","fuck","");
-console.log(answer);
+// let answer = prompt("Hells Bells","fuck","");
+// console.log(answer);
